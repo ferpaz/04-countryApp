5db8c0009db54e9a711d411c7190212351cb3553
Fernando Paz <ferpaz64@gmail.com>
2024-04-30 15:14:33 -0600

    Se creo un routing básico para tres páginas desde el sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './shared/pages/home-page/home-page.component';
 import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomePageComponent, },
+  { path: '', component: HomePageComponent, },
+  { path: 'contact', component: ContactPageComponent, },
   { path: 'about', component: AboutPageComponent, },
 
   // Redirect to home page when route does not exist
-  { path: '**', redirectTo: '/home'}
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({
