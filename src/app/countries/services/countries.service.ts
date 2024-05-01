b06d6bef9cb0336f6b557da26ce43b088d200721
Fernando Paz <ferpaz64@gmail.com>
2024-04-30 19:31:18 -0600

    refactoreo del service para evitar codigo duplicado, ordenamiento alfabetico por nombre de pais

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 
 import { Country } from '../interfaces/country';
 
@@ -12,31 +12,30 @@ export class CountriesService {
 
   constructor(private httClient: HttpClient) { }
 
+
+  public searchByCapital(criteria: string): Observable<Country[]> {
+    return this.searchBy(criteria, 'capital');
+  }
+
+  public searchByCountry(criteria: string): Observable<Country[]> {
+    return this.searchBy(criteria, 'name');
+  }
+
+  public searchByRegion(criteria: string): Observable<Country[]> {
+    return this.searchBy(criteria, 'region');
+  }
+
+
   // Retorna un Observable con un array de objetos de tipo Country
   // hasta que no hay algun componente suscrito a este Observable
   // no se realiza la petición HTTP
 
-  public searchByCapital(criteria: string): Observable<Country[]> {
-    const url = `${this.apiUrl}/capital/${criteria}`;
+  private searchBy(criteria: string, endpoint: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/${endpoint}/${criteria}`;
     return this.httClient.get<Country[]>(url)
       .pipe(
-        catchError(() => of([]))
-      );
-  }
-
-  public searchByCountry(criteria: string): Observable<Country[]> {
-    const url = `${this.apiUrl}/name/${criteria}`;
-    return this.httClient.get<Country[]>(url)
-      .pipe(
-        catchError(() => of([]))
-      );
-  }
-
-  public searchByRegion(criteria: string): Observable<Country[]> {
-    const url = `${this.apiUrl}/region/${criteria}`;
-    return this.httClient.get<Country[]>(url)
-      .pipe(
-        catchError(() => of([]))
+        catchError(() => of([])),
+        map(countries => countries.sort((a, b) => a.name.common.localeCompare(b.name.common)))
       );
   }
 }
